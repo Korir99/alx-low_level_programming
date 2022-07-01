a6518b52b16d75a707dd4cc461a70a15b65e4cf9
Maureen Korir <korirmoureen99@gmail.com>
2022-07-01 20:35:13 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - prints single digits
+*
+* Return: 0 (success)
+*/
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
