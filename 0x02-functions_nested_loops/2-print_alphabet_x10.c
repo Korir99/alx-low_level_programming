b0073b1d5d368bf51b945814c22c288feb8ab06d
Maureen Korir <korirmoureen99@gmail.com>
2022-07-05 15:51:28 +0000

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,14 @@
 */
 void print_alphabet_x10(void);
 {
-	char i, j;
+	int i, j;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
-			_putchar('10', j);
+			_putchar((**10), j);
 		}
 		_putchar('\n');
 	}
-
-
-
 }
