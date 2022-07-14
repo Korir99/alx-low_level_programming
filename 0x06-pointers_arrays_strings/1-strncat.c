ebf1e40071c3e9544f6af78ccb75393295f8671f
Korir99 <korirmoureen99@gmail.com>
2022-07-14 15:36:48 +0000

    I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,19 +10,6 @@
 **/
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-
-	for (i = 0; dest[i] != 0; i++)
-	{
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		if (j < n)
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-	dest[i] = '\0';
+	strncat(dest, src, n);
 	return (dest);
 }
