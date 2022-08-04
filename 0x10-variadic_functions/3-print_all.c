502cc3482eafdf4b7a23f574f4367c8c8bf05fc4
Korir99 <korirmoureen99@gmail.com>
2022-08-04 14:07:00 +0000

    To be is a to be the value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,83 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+/**
+* print_char - prints characters
+* @ap: argument
+*/
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+/**
+* print_int - print integer
+* @ap: argument
+*/
+void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+/**
+* print_float - prints float
+* @ap: argument
+*/
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+/**
+* print_string - prints string
+* @ap: argument
+*/
+void print_string(va_list ap)
+{
+	char *s = va_arg(ap, char *);
+
+	if (s != NULL)
+	{
+		printf("%s", s);
+		return;
+	}
+	printf("(nil)");
+}
+/**
+* print_all - prints anything
+* @format: lists types of arguements
+* Return: void
+*/
+void print_all(const char * const format, ...)
+{
+	var_t print[] = {
+	{'c', print_char},
+	{'i', print_int},
+	{'f', print_float},
+	{'s', print_string},
+	{'\0', NULL}
+	};
+	unsigned int i, j;
+	va_list ap;
+
+	char *separator = "";
+
+	va_start(ap, format);
+	i = 0;
+	while (format[i] && format != NULL)
+	{
+		j = 0;
+		while (print[j].var)
+		{
+			if (print[j].var == format[i])
+			{
+				printf("%s", separator);
+				print[j].meth(ap);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,22 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+typedef struct var
+{
+	char var;
+	void (*meth)(va_list);
+} var_t;
+void print_char(va_list ap);
+void print_int(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
 
 #endif
